6a0d2676c8af04f41e8382b6168f47d091713846
Narek2481 <narekengibaryan.1@mail.ru>
2022-06-18 01:04:26 +0400

    react

--- src/Components/Secton/Block_4.js
+++ src/Components/Secton/Block_4.js
@@ -6,74 +6,15 @@ import povr from '../../img/povr.png';
 import btn_false from '../../img/Ellipse 1 (1).png';
 import btn_true from '../../img/Ellipse 2.png';
 import hover from '../../img/block4_hover.png';
-import { useEffect, useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 
-export default function Block_4() {
-
-    const button_src = [btn_false, btn_true, btn_false];
-    const slider_arr = [
-        {
-            h2: 'hopar',
-            li1: 'Шаурма из курицы ',
-            li2: 'Шаурма из свинины ',
-            li3: 'Шаурма люля из курицы',
-            li4: 'Шаурма люля из телятины ',
-            li5: 'Шаурма из индейки',
-            li6: 'Шаурма из люля из индейки ',
-            li7: 'Шаурма из говядины ',
-            li8: 'Шаурма цезарули',
-            span1: '160р ',
-            span2: '180р ',
-            span3: '180р ',
-            span4: '200р ',
-            span5: '190р',
-            span6: '200р ',
-            span7: '190р',
-            span8: '180р ',
-            span_red: '450 гр'
-        }, {
-            h2: 'Шаурма на углях ',
-            li1: 'Шаурма из курицы ',
-            li2: 'Шаурма из свинины ',
-            li3: 'Шаурма люля из курицы',
-            li4: 'Шаурма люля из телятины ',
-            li5: 'Шаурма из индейки',
-            li6: 'Шаурма из люля из индейки ',
-            li7: 'Шаурма из говядины ',
-            li8: 'Шаурма цезарули',
-            span1: '160р ',
-            span2: '180р ',
-            span3: '180р ',
-            span4: '200р ',
-            span5: '190р',
-            span6: '200р ',
-            span7: '190р',
-            span8: '180р ',
-            span_red: '450 гр'
-        }, {
-            h2: 'Шаурма на углях ',
-            li1: 'Шаурма из курицы ',
-            li2: 'Шаурма из свинины ',
-            li3: 'Шаурма люля из курицы',
-            li4: 'Шаурма люля из телятины ',
-            li5: 'Шаурма из индейки',
-            li6: 'Шаурма из люля из индейки ',
-            li7: 'Шаурма из говядины ',
-            li8: 'Шаурма цезарули',
-            span1: '160р ',
-            span2: '180р ',
-            span3: '180р ',
-            span4: '200р ',
-            span5: '190р',
-            span6: '200р ',
-            span7: '190р',
-            span8: '180р ',
-            span_red: '450 гр'
-        }];
-
-    let chack = 1;
-    let props = slider_arr[chack]
+export default function Block_4({ slider_arr }) {
+    console.log(slider_arr)
+    let props = slider_arr[0];
+    let props2 = slider_arr[1];
+    let props3 = slider_arr[2];
 
     return (
         <div className="block_4">
@@ -105,76 +46,191 @@ export default function Block_4() {
                 </div>
             </div>
             <div className='block_4_child_2'>
-                <div className='block_4_child_2_child'>
-                    <div>
-                        <img src={povr} />
-                    </div>
-                    <div>
-                        <h2>{props.h2}</h2>
-                        <ul>
-                            <li>
-                                {props.li1}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span1}
-                                </span>
-                            </li>
-                            <li>{props.li2}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span2}
-                                </span>
-                            </li>
-                            <li>{props.li3}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span3}
-                                </span>
-                            </li>
-                            <li>{props.li4}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span4}
-                                </span>
-                            </li>
-                            <li>{props.li5}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span5}
-                                </span>
-                            </li>
-                            <li>{props.li6}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span6}
-                                </span>
-                            </li>
-                            <li>{props.li7}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span7}
-                                </span>
-                            </li>
-                            <li>{props.li8}
-                                <span>
-                                    <span>{props.span_red}</span>
-                                    {props.span8}
-                                </span>
-                            </li>
-                        </ul>
-                        <div className='btn_container'>
-                            <button className='btn1'>
-                                <img src={button_src[0]} />
-                            </button>
-                            <button className='btn2'>
-                                <img src={button_src[1]} />
-                            </button>
-                            <button className='btn3'>
-                                <img src={button_src[2]} />
-                            </button>
+                <Carousel>
+                    <div className='block_4_child_2_child'>
+                        <div>
+                            <img src={povr} />
+                        </div>
+                        <div>
+                            <h2>{props.h2}</h2>
+                            <ul>
+                                <li>
+                                    {props.li1}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span1}
+                                    </span>
+                                </li>
+                                <li>{props.li2}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span2}
+                                    </span>
+                                </li>
+                                <li>{props.li3}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span3}
+                                    </span>
+                                </li>
+                                <li>{props.li4}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span4}
+                                    </span>
+                                </li>
+                                <li>{props.li5}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span5}
+                                    </span>
+                                </li>
+                                <li>{props.li6}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span6}
+                                    </span>
+                                </li>
+                                <li>{props.li7}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span7}
+                                    </span>
+                                </li>
+                                <li>{props.li8}
+                                    <span>
+                                        <span>{props.span_red}</span>
+                                        {props.span8}
+                                    </span>
+                                </li>
+                            </ul>
+
                         </div>
                     </div>
-                </div>
+
+
+                    <div className='block_4_child_2_child'>
+                        <div>
+                            <img src={povr} />
+                        </div>
+                        <div>
+                            <h2>{props2.h2}</h2>
+                            <ul>
+                                <li>
+                                    {props2.li1}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span1}
+                                    </span>
+                                </li>
+                                <li>{props2.li2}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span2}
+                                    </span>
+                                </li>
+                                <li>{props2.li3}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span3}
+                                    </span>
+                                </li>
+                                <li>{props2.li4}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span4}
+                                    </span>
+                                </li>
+                                <li>{props2.li5}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span5}
+                                    </span>
+                                </li>
+                                <li>{props2.li6}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span6}
+                                    </span>
+                                </li>
+                                <li>{props2.li7}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span7}
+                                    </span>
+                                </li>
+                                <li>{props2.li8}
+                                    <span>
+                                        <span>{props2.span_red}</span>
+                                        {props2.span8}
+                                    </span>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    <div className='block_4_child_2_child'>
+                        <div>
+                            <img src={povr} />
+                        </div>
+                        <div>
+                            <h2>{props3.h2}</h2>
+                            <ul>
+                                <li>
+                                    {props3.li1}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span1}
+                                    </span>
+                                </li>
+                                <li>{props3.li2}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span2}
+                                    </span>
+                                </li>
+                                <li>{props3.li3}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span3}
+                                    </span>
+                                </li>
+                                <li>{props3.li4}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span4}
+                                    </span>
+                                </li>
+                                <li>{props3.li5}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span5}
+                                    </span>
+                                </li>
+                                <li>{props3.li6}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span6}
+                                    </span>
+                                </li>
+                                <li>{props3.li7}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span7}
+                                    </span>
+                                </li>
+                                <li>{props3.li8}
+                                    <span>
+                                        <span>{props3.span_red}</span>
+                                        {props3.span8}
+                                    </span>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
